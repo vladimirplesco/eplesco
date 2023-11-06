8f5b1c816edb4efcd411f7afe55b2ea83a7053df
Vladimir Plesco <pleshco@deeplace.md>
2023-11-06 10:37:16 +0200

    new version

--- src/assets/js/lax.js
+++ src/assets/js/lax.js
@@ -2,17 +2,54 @@ import lax from "lax.js";
 
 lax.init()
 
-    // Add a driver that we use to control our animations
-    lax.addDriver('scrollY', function () {
-      return window.scrollY
-    })
+lax.addDriver("scrollY", () => window.scrollY);
 
-    // Add animation bindings to elements
-    lax.addElements('.selector', {
-      scrollY: {
-        translateX: [
-          ["elInY", "elCenterY", "elOutY"],
-          [0, 'screenWidth/2', 'screenWidth'],
-        ]
-      }
-    })
+lax.addElements(".first-name", {
+  scrollY: {
+    opacity: [
+      [0, "screenHeight/2"],
+      ["Math.random() * (0.7 - 0.3) + 0.3", 1],
+    ],
+    rotate: [
+      [0, "screenHeight/2"],
+      ["Math.random() * (180 - (-180)) + (-180)", 0],
+    ],
+    scale: [
+      [0, "screenHeight/2"],
+      ["Math.random() * (3 - 0.5) + 0.5", 1],
+    ],
+    translateY: [
+      [0, "screenHeight/2"],
+      ["Math.random() * ((screenHeight-40)*(-1) - (elHeight+80)*(-1)) + (elHeight+80)*(-1)", 0],
+    ],
+    translateX: [
+      [0, "screenHeight/2"],
+      ["Math.random() * (((screenWidth - (elWidth+40)*5)/2) - ((screenWidth - (elWidth+40)*5)/2)*(-1)) + ((screenWidth - (elWidth+40)*5)/2)*(-1)", 0],
+    ]
+  }
+});
+
+lax.addElements(".last-name", {
+  scrollY: {
+    opacity: [
+      [0, "screenHeight/2"],
+      ["Math.random() * (0.7 - 0.3) + 0.3", 1],
+    ],
+    rotate: [
+      [0, "screenHeight/2"],
+      ["Math.random() * (180 - (-180)) + (-180)", 0],
+    ],
+    scale: [
+      [0, "screenHeight/2"],
+      ["Math.random() * (3 - 0.5) + 0.5", 1],
+    ],
+    translateY: [
+      [0, "screenHeight/2"],
+      ["Math.random() * ((screenHeight+elHeight)*(-1) - (elHeight*2+120)*(-1)) + (elHeight*2+120)*(-1)", 0],
+    ],
+    translateX: [
+      [0, "screenHeight/2"],
+      ["Math.random() * (((screenWidth - (elWidth+40)*6)/2) - ((screenWidth - (elWidth+40)*6)/2)*(-1)) + ((screenWidth - (elWidth+40)*6)/2)*(-1)", 0],
+    ]
+  }
+});
